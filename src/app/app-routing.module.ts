7250068478178ce3319b817d1d5bb78db431cdc0
Nikolai Baishev <baishev2008@gmail.com>
2023-01-21 19:08:41 +0600

    Implement routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from "./pages/home/home.component";
+import {CharacterComponent} from "./pages/character/character.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "home",
+    component: HomeComponent,
+  },
+  {
+    path: "home/:id",
+    component: CharacterComponent
+  },
+  {
+    path: "**",
+    redirectTo: "/home"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
